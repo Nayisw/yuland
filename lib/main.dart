878f9bd066299a17cb9c04dfcb474db2bc187390
Nayisw <mrprogaming003@gmail.com>
2023-10-15 20:13:11 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:yuland/screens/home.dart';
+import 'package:yuland/screens/notes_screen.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Notes',
-      home: HomeScreen(),
+    return MaterialApp(
+      title: 'Yuland Notes',
+      debugShowCheckedModeBanner: false,
+      home: NotesScreen(),
     );
   }
 }
